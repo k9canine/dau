b7f1598f8c407026d07dc5b7d43c9ef37b91f367
Katherine Li <katherineli03.kkl@gmail.com>
2023-01-14 16:25:10 -0500

    updated the display to use ufo

--- DAU-API/GameTest/GameTest.cpp
+++ DAU-API/GameTest/GameTest.cpp
@@ -3,8 +3,8 @@
 //------------------------------------------------------------------------
 #include "stdafx.h"
 //------------------------------------------------------------------------
-#include <windows.h> 
-#include <math.h>  
+#include <windows.h>
+#include <math.h>
 //------------------------------------------------------------------------
 #include "app\app.h"
 //------------------------------------------------------------------------
@@ -29,14 +29,11 @@ void Init()
 {
 	//------------------------------------------------------------------------
 	// Example Sprite Code....
-	testSprite = App::CreateSprite(".\\TestData\\Test.bmp", 8, 3);
-	testSprite->SetPosition(400.0f, 400.0f);
-	float speed = 1.0f / 15.0f;
-	testSprite->CreateAnimation(ANIM_BACKWARDS, speed, { 0,1,2,3,4,5,6,7 });
-	testSprite->CreateAnimation(ANIM_LEFT, speed, { 8,9,10,11,12,13,14,15 });
-	testSprite->CreateAnimation(ANIM_RIGHT, speed, { 16,17,18,19,20,21,22,23 });
-	testSprite->CreateAnimation(ANIM_FORWARDS, speed, { 24,25,26,27,28,29,30,31 });
-	testSprite->SetScale(1.0f);
+	testSprite = App::CreateSprite(".\\TestData\\ufo.bmp", 1, 6);
+	testSprite->SetPosition(500.0f, 50.0f);
+	float speed = 3.0f / 15.0f;
+	testSprite->CreateAnimation(ANIM_BACKWARDS, speed, {1, 2, 3, 4, 5});
+	testSprite->SetScale(2.0f);
 	//------------------------------------------------------------------------
 }
 
@@ -46,6 +43,7 @@ void Init()
 //------------------------------------------------------------------------
 void Update(float deltaTime)
 {
+	testSprite->SetAnimation(ANIM_BACKWARDS);
 	//------------------------------------------------------------------------
 	// Example Sprite Code....
 	testSprite->Update(deltaTime);
@@ -63,9 +61,13 @@ void Update(float deltaTime)
 		float x, y;
 		testSprite->GetPosition(x, y);
 		x -= 1.0f;
+
+		// if out of bounds (x - 1.0f, y)
+		// 	do nothing
+		// else x -= 1.0f
 		testSprite->SetPosition(x, y);
 	}
-	if (App::GetController().GetLeftThumbStickY() > 0.5f)
+	if (App::GetController().GetLeftThumbStickY() < -0.5f)
 	{
 		testSprite->SetAnimation(ANIM_FORWARDS);
 		float x, y;
@@ -73,7 +75,7 @@ void Update(float deltaTime)
 		y += 1.0f;
 		testSprite->SetPosition(x, y);
 	}
-	if (App::GetController().GetLeftThumbStickY() < -0.5f)
+	if (App::GetController().GetLeftThumbStickY() > 0.5f)
 	{
 		testSprite->SetAnimation(ANIM_BACKWARDS);
 		float x, y;
@@ -81,41 +83,21 @@ void Update(float deltaTime)
 		y -= 1.0f;
 		testSprite->SetPosition(x, y);
 	}
-	if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_UP, false))
-	{
-		testSprite->SetScale(testSprite->GetScale() + 0.1f);
-	}
-	if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_DOWN, false))
-	{
-		testSprite->SetScale(testSprite->GetScale() - 0.1f);
-	}
-	if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_LEFT, false))
-	{
-		testSprite->SetAngle(testSprite->GetAngle() + 0.1f);
-	}
-	if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_RIGHT, false))
-	{
-		testSprite->SetAngle(testSprite->GetAngle() - 0.1f);
-	}
-	if (App::GetController().CheckButton(XINPUT_GAMEPAD_A, true))
-	{
-		testSprite->SetAnimation(-1);
-	}
 	//------------------------------------------------------------------------
-	// Sample Sound.
+	// Sample Sound. - USE FOR SHOOTING ENEMIES
 	//------------------------------------------------------------------------
-	if (App::GetController().CheckButton(XINPUT_GAMEPAD_B, true))
-	{
-		App::PlaySound(".\\TestData\\Test.wav");
-	}
+	// if (App::GetController().CheckButton(XINPUT_GAMEPAD_B, true))
+	// {
+	// 	App::PlaySound(".\\TestData\\Test.wav");
+	// }
 }
 
 //------------------------------------------------------------------------
-// Add your display calls here (DrawLine,Print, DrawSprite.) 
-// See App.h 
+// Add your display calls here (DrawLine,Print,DrawSprite.)
+// See App.h
 //------------------------------------------------------------------------
 void Render()
-{	
+{
 	//------------------------------------------------------------------------
 	// Example Sprite Code....
 	testSprite->Draw();
@@ -124,34 +106,33 @@ void Render()
 	//------------------------------------------------------------------------
 	// Example Text.
 	//------------------------------------------------------------------------
-	App::Print(100, 100, "Sample Text");
+	App::Print(20, 730, "Level: 1     Score: 0");
 
 	//------------------------------------------------------------------------
 	// Example Line Drawing.
 	//------------------------------------------------------------------------
-	static float a = 0.0f;
-	float r = 1.0f;
-	float g = 1.0f;
-	float b = 1.0f;
-	a += 0.1f;
-	for (int i = 0; i < 20; i++)
-	{
-
-		float sx = 200 + sinf(a + i * 0.1f)*60.0f;
-		float sy = 200 + cosf(a + i * 0.1f)*60.0f;
-		float ex = 700 - sinf(a + i * 0.1f)*60.0f;
-		float ey = 700 - cosf(a + i * 0.1f)*60.0f;
-		g = (float)i / 20.0f;
-		b = (float)i / 20.0f;
-		App::DrawLine(sx, sy, ex, ey,r,g,b);
-	}
+	// static float a = 0.0f;
+	// float r = 1.0f;
+	// float g = 1.0f;
+	// float b = 1.0f;
+	// a += 0.1f;
+	// // for (int i = 0; i < 20; i++)
+	// // {
+	// float sx = 200 + sinf(a + i * 0.1f) * 60.0f;
+	// float sy = 200 + cosf(a + i * 0.1f) * 60.0f;
+	// float ex = 700 - sinf(a + i * 0.1f) * 60.0f;
+	// float ey = 700 - cosf(a + i * 0.1f) * 60.0f;
+	// g = (float)i / 20.0f;
+	// b = (float)i / 20.0f;
+	// App::DrawLine(sx, sy, ex, ey, r, g, b);
+	// // }
 }
 //------------------------------------------------------------------------
 // Add your shutdown code here. Called when the APP_QUIT_KEY is pressed.
 // Just before the app exits.
 //------------------------------------------------------------------------
 void Shutdown()
-{	
+{
 	//------------------------------------------------------------------------
 	// Example Sprite Code....
 	delete testSprite;
